75a6233f62e08ff86f2fc42314b106e01931caa4
Virginia Álvarez <virginiaalvarez@MacBook-Pro-de-Virginia.local>
2017-11-06 21:02:26 +0100

    1est. iteration

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,36 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "up";
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
-}
+  start() {
+   let go = setInterval (() => {
+    this.update();
+  }, 1000);
+};
 
+  stop() {
+  clearInterval(go); }
+
+  update() {
+  this.log(); }
+
+  _passengersEnter() { }
+
+  _passengersLeave() { }
+
+  floorUp() { }
+
+  floorDown() { }
+
+  call() {
+   }
+
+  log() {
+  console.log("direction " + this.direction + "floor " + this.floor);
+};
+
+}
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,8 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+let elevator = new Elevator();
+//elevator.update();
+elevator.start();
+elevator.stop();
